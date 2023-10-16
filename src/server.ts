3d3e89638d0c129e50b425d2625cf9fad57a2a4b
Shiva Kumar Mangina <shiva@transak.com>
2023-10-16 14:50:21 +0530

    feat: Add Hapi Node typescript boilerplate
    refactor: Remove unwanted files
    
    test commit

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import Hapi from '@hapi/hapi';
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => {
+      return {
+        message: 'Server is running!',
+      };
+    },
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+export default init;
